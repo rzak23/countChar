534c75d577179c9fb3377d16b877bb2474e25a16
rzak23 <reza.akbar2396@gmail.com>
2020-12-15 10:19:54 +0800

    add autoupdate

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-const { app, BrowserWindow, Menu } = require('electron');
+const { app, BrowserWindow, Menu, autoUpdater, dialog } = require('electron');
+const server = 'https://vercel.com/rzak23/count-char';
+const url = `${server}/update/${process.platform}/${app.getVersion()}`;
 
 function createWindow () {
   const win = new BrowserWindow({
@@ -26,4 +28,28 @@ app.on('activate', () => {
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow()
   }
+})
+
+autoUpdater.setFeedURL({ url })
+
+setInterval(() => {
+  autoUpdater.checkForUpdates()
+}, 60000)
+
+autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
+  const dialogOpts = {
+    type: 'info',
+    buttons: ['Restart', 'Later'],
+    title: 'Application Update',
+    message: process.platform === 'win32' ? releaseNotes : releaseName,
+    detail: 'A new version has been downloaded. Restart the application to apply the updates.'
+  }
+
+  dialog.showMessageBox(dialogOpts).then((returnValue) => {
+    if (returnValue.response === 0) autoUpdater.quitAndInstall()
+  })
+})
+autoUpdater.on('error', message => {
+  console.error('There was a problem updating the application')
+  console.error(message)
 })
